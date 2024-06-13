c24dfe212edd7bfc62587324ed3d1ea1c7c2e8f7
Your Name <you@example.com>
2024-06-13 10:46:58 -0600

    calculate distance between two points

--- mySubmission.go
+++ mySubmission.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func calculateDistance() {
-    
+func roundToDecimalPlaces(num float64, decimalPlaces int) float64 {
+    // https://gosamples.dev/round-float/
+    ratio := math.Pow(10, float64(decimalPlaces))
+    return math.Round(num*ratio) / ratio
+}
+
+func calculateDistance(x1 float64, y1 float64, x2 float64, y2 float64) float64 {
+    d1 := x2 - x1
+    d2 := y2 - y1
+    distance := math.Sqrt((d1*d1) + (d2*d2))
+    distanceRounded := roundToDecimalPlaces(distance, 2)
+    return distanceRounded
 }
 
 func main() {
--- mySubmission_test.go
+++ mySubmission_test.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-	"strconv"
 	"testing"
 )
 
-func TestCalculateDistance(t *testing.T) {
+func TestPrecisionRoundedToTens(t *testing.T) {
 
-    got := true
-    want := true
+    got := roundToDecimalPlaces(1.2345, 2)
+    want := 1.23
 
     if got != want {
-        fgot := strconv.FormatBool(got)
-        fwant := strconv.FormatBool(want)
-        t.Errorf("got %q, wanted %q", fgot, fwant)
+        t.Errorf("got %f, wanted %f", got, want)
+    }
+}
+
+func TestPrecisionRoundedToHundreds(t *testing.T) {
+
+    got := roundToDecimalPlaces(1.2345, 3)
+    want := 1.235
+
+    if got != want {
+        t.Errorf("got %f, wanted %f", got, want)
+    }
+}
+
+func TestCalculateDistanceBetween2Points(t *testing.T) {
+
+    got := calculateDistance(0.0, 0.0, 50.0, 50.0)
+    want := 70.71
+
+    if got != want {
+        t.Errorf("got %f, wanted %f", got, want)
+    }
+}
+
+func TestCalculateDistanceBetween3Points(t *testing.T) {
+    // @TODO - represent points as struct
+    distanceToA := calculateDistance(0.0, 0.0, 50.0, 50.0)
+    distanceToB := calculateDistance(50.0, 50.0, 100.0, 100.0)
+
+    got := distanceToA + distanceToB
+    want := 141.42
+
+    if got != want {
+        t.Errorf("got %f, wanted %f", got, want)
     }
 }
