e9c3c02f6de38f08e4fbc48d3698e6adfc8f1761
Your Name <you@example.com>
2024-06-13 11:51:20 -0600

    parse loads from input file

--- mySubmission.go
+++ mySubmission.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math"
+	"os"
 )
+
+type Point struct {
+    X float64
+    Y float64
+}
+
+type Load struct {
+    Number int
+    Start Point
+    End Point
+}
+
 
 func roundToDecimalPlaces(num float64, decimalPlaces int) float64 {
     // https://gosamples.dev/round-float/
@@ -19,6 +34,69 @@ func calculateDistance(x1 float64, y1 float64, x2 float64, y2 float64) float64 {
     return distanceRounded
 }
 
+func parseLoadFromString(line string) Load {
+    // https://pkg.go.dev/fmt#example-Sscanf
+
+    // Extract the load number
+    var number int
+	_, err := fmt.Sscanf(line, "%d", &number)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	// Extract the start and end points
+	var startX, startY, endX, endY float64
+	_, err = fmt.Sscanf(line, "%d (%f,%f) (%f,%f)",
+		&number, &startX, &startY, &endX, &endY)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+    load := Load{
+		Number: number,
+		Start: Point{
+			X: startX,
+			Y: startY,
+		},
+		End: Point{
+			X: endX,
+			Y: endY,
+		},
+	}
+
+    return load
+}
+
+func readInput(fileName string) []Load {
+    // @TODO - return err if file not exists, or on parse error
+    file, err := os.Open(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    loads := []Load{}
+    isFirstLine := true
+    scanner := bufio.NewScanner(file)
+
+    for scanner.Scan() {
+        if isFirstLine {
+            isFirstLine = false
+            continue // Skip header
+        }
+        load := parseLoadFromString(scanner.Text())
+        loads = append(loads, load)
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+    return loads
+}
+
 func main() {
-    fmt.Println("test")
+    // @TODO - error check args, ensure filename arg is provided
+    args := os.Args
+    fileName := args[1]
+    readInput(fileName)
 }
--- mySubmission_test.go
+++ mySubmission_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// @TODO - it would be better to parametrize these test inputs
+
 func TestPrecisionRoundedToTens(t *testing.T) {
 
     got := roundToDecimalPlaces(1.2345, 2)
@@ -35,7 +37,7 @@ func TestCalculateDistanceBetween2Points(t *testing.T) {
 }
 
 func TestCalculateDistanceBetween3Points(t *testing.T) {
-    // @TODO - represent points as struct
+    // @TODO - use new Point struct
     distanceToA := calculateDistance(0.0, 0.0, 50.0, 50.0)
     distanceToB := calculateDistance(50.0, 50.0, 100.0, 100.0)
 
@@ -44,5 +46,32 @@ func TestCalculateDistanceBetween3Points(t *testing.T) {
 
     if got != want {
         t.Errorf("got %f, wanted %f", got, want)
+    }
+}
+
+func TestParseLoadFromString(t *testing.T) {
+
+    str := "200 (26.177425683364,117.72887590630494) (-49.79976850340818,59.79329676250279)"
+    load := parseLoadFromString(str)
+    wantLoadNumber := 200
+    wantLoadStartX := 26.177425683364
+    wantLoadStartY := 117.72887590630494
+    wantLoadEndX := -49.79976850340818
+    wantLoadEndY := 59.79329676250279
+
+    if load.Number != wantLoadNumber {
+        t.Errorf("got %d, wanted %d", load.Number, wantLoadNumber)
+    }
+    if load.Start.X != wantLoadStartX {
+        t.Errorf("got %f, wanted %f", load.Start.X, wantLoadStartX)
+    }
+    if load.Start.Y != wantLoadStartY {
+        t.Errorf("got %f, wanted %f", load.Start.X, wantLoadStartX)
+    }
+    if load.End.X != wantLoadEndX {
+        t.Errorf("got %f, wanted %f", load.Start.X, wantLoadStartX)
+    }
+    if load.End.Y != wantLoadEndY {
+        t.Errorf("got %f, wanted %f", load.Start.X, wantLoadStartX)
     }
 }
